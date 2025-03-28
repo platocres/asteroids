9b9df4faea2e985a6e8beb6e23cf5dbd7f34e69c
platocres <platocres@gmail.com>
2025-03-28 11:04:18 -0400

    B: Set pygame FPS and added a constant

--- main.py
+++ main.py
@@ -5,18 +5,26 @@ import pygame
 from constants import *
 
 def main():
-    pygame.init
+    pygame.init()
+    pygame.time.Clock
+    clock = pygame.time.Clock()     # Create a Clock instance outside the loop
+    dt = 0                          # Delta time initialized to 0
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while 0 == 0:
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill
-        pygame.display.flip
+            
+        screen.fill((0, 0, 0))      # Fills the screen with black
+        pygame.display.flip()       # Updates the screen with what we've drawn
+        dt = clock.tick(60) / 1000  # Calculate delta time, in seconds
+        
+        
+
 
 if __name__ == "__main__":
     main()
