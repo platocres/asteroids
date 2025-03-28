842d736fcccca3521259d6a4152cc9878af89f5c
platocres <platocres@gmail.com>
2025-03-28 13:59:57 -0400

    D: Added upward and downward movement for the player

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 import circleshape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 
 class Player(circleshape.CircleShape):
     def __init__(self, x, y):
@@ -16,18 +16,27 @@ class Player(circleshape.CircleShape):
         c = self.position - forward * self.radius + right
         return [a, b, c]
     
-    #Drawing the triangle for the player
+    # Drawing the triangle representing the player
     def draw(self, screen):
         pygame.draw.polygon(screen, 'white', self.triangle(), 2)
     
-    #Rotates the player triangle
+    # Rotates the player
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
     
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_a]:
-            self.rotate(-dt)  # Use self. and pass negative dt
-        if keys[pygame.K_d]:
-            self.rotate(dt)   # Use self. here too
+        if keys[pygame.K_a]:    # Rotate left
+            self.rotate(-dt)    
+        if keys[pygame.K_d]:    # Rotate right
+            self.rotate(dt)   
+        if keys[pygame.K_w]:    # Move up
+            self.move(dt)
+        if keys[pygame.K_s]:    # Move down
+            self.move(-dt)
+
+    # Moves the player forward
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
