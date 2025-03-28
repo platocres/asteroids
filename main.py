60f5b3e20eaeb7eb9e7a8624b867bc1b27dbfae3
platocres <platocres@gmail.com>
2025-03-28 13:39:57 -0400

    C: Added functionality for rotating the player triangle

--- main.py
+++ main.py
@@ -2,27 +2,46 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+import sys
 from constants import *
+from player import Player
 
 def main():
+    #Initialize Pygame
     pygame.init()
-    pygame.time.Clock
+
     clock = pygame.time.Clock()     # Create a Clock instance outside the loop
     dt = 0                          # Delta time initialized to 0
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    #Create the screen
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while True:
+
+    # Create the player
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
+
+    running = True
+    while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
-            
+                running = False
+
+        # proper order of operations (fill screen → update → draw → flip display)    
         screen.fill((0, 0, 0))      # Fills the screen with black
+
+        player.update(dt)           # Enables player rotation, must initialize prior the drawing the character
+
+        player.draw(screen)         # Draws the triangle player, needs to happen after filling the screen but prior to flipping it.
+
         pygame.display.flip()       # Updates the screen with what we've drawn
-        dt = clock.tick(60) / 1000  # Calculate delta time, in seconds
-        
+        dt = clock.tick(60) / 1000  # Calculate delta time, in seconds, and set framerate to 60 FPS
+    
+    #Clean up
+    pygame.quit()
+    sys.exit()
+      
         
 
 
